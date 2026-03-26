f0549bafc89a70d697b9b66d833589051442717f
Senzaj <sanamistik@mail.ru>
2026-03-26 15:25:24 +0300

    added profiles without png

--- src/js/Constants/profilesList.js
+++
@@ -1,5 +0,0 @@
-const profiles = [
-	{ name: "Олег", sex: "♂️", age: "23 года", img: "assets/images/profiles/oleg.png", bio: "люблю кофе :3" },
-	{ name: "Евпатий", sex: "♂️", age: "25 лет", img: "assets/images/profiles/evpatij.png", bio: "люблю грязный спорт" }];
-
-const responseVariants = ["?", "интересно...", "ок", "угу"];
---
+++ src/js/profilesList.js
@@ -0,0 +1,8 @@
+const profiles = [
+	{ name: "Олег", sex: "♂️", age: "23 года", img: "assets/images/profiles/oleg.png", bio: "люблю тёплый кофе :3" },
+	{ name: "Антошка", sex: "♂️", age: "19 лет", img: "assets/images/profiles/oleg.png", bio: "люблю компотик >_<" },
+	{ name: "Валентина", sex: "♂️", age: "43 года", img: "assets/images/profiles/oleg.png", bio: "люблю морсик :)" },
+	{ name: "مرحبًا", sex: "♂️", age: "23 года", img: "assets/images/profiles/oleg.png", bio: "ничего не люблю >:|" },
+	{ name: "Евпатий", sex: "♂️", age: "68 лет", img: "assets/images/profiles/evpatij.png", bio: "люблю грязный спорт" }];
+
+const responseVariants = ["?", "интересно...", "ок", "👎","👍","✊","💤"];
